191050c43819299168847a4764aabcebad158a7d
Nishant Chauhan <nishantkr0210@gmail.com>
2020-09-24 19:03:16 +0530

    Attendence Checked

--- employee.java
+++ employee.java
@@ -2,6 +2,12 @@ public class employee{
 
    public static void main(String[]  args){
        System.out.println("Welcome to Employee Wage Computation Program");
+
+    double attendence = Math.floor(Math.random()*10) % 2;
+    if(attendence==1)
+        System.out.println("Employee Is Present");
+    else
+        System.out.println("Employee Is Absent");
    }
 }
 

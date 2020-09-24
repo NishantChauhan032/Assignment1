7a1b213d21e005b63c1d3dcf49613de32b352a63
Nishant Chauhan <nishantkr0210@gmail.com>
2020-09-24 19:29:26 +0530

    Daily Wage Calculated

--- employee.java
+++ employee.java
@@ -2,6 +2,17 @@ public class employee{
 
    public static void main(String[]  args){
        System.out.println("Welcome to Employee Wage Computation Program");
-   }
+   int hours=0; int wage=0;
+   double presence = Math.floor(Math.random()*10) % 2;
+       if(presence==1)
+          hours=8;
+else
+    hours=0;
+
+   wage= hours*20;
+    
+    System.out.println("Daily Wage Of Employee Is : " + wage);
+
+  }
 }
 

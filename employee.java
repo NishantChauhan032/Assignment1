123dba980ac810f5c00a6aa22dc2fc02ff6262b7
Nishant Chauhan <nishantkr0210@gmail.com>
2020-09-25 16:48:11 +0530

    Part time wage added

--- employee.java
+++ employee.java
@@ -3,15 +3,19 @@ public class employee{
    public static void main(String[]  args){
        System.out.println("Welcome to Employee Wage Computation Program");
    int hours=0; int wage=0;
-   double presence = Math.floor(Math.random()*10) % 2;
-       if(presence==1)
+   double presence = Math.floor(Math.random()*10) % 3;
+       if(presence==1){
           hours=8;
-else
-    hours=0;
+          }
+      else if(presence==2){
+       hours=4;
+          }
+      else
+       hours=0;
 
-   wage= hours*20;
+     wage= hours*20;
     
-    System.out.println("Daily Wage Of Employee Is : " + wage);
+     System.out.println("Daily Wage Of Employee Is : " + wage);
 
   }
 }

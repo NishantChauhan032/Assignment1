063248db2ee9e3aed47646faae381e1407244d16
Nishant Chauhan <nishantkr0210@gmail.com>
2020-09-25 18:05:56 +0530

    Wage for a month Calculated

--- employee.java
+++ employee.java
@@ -2,14 +2,17 @@ public class employee{
    public static final int is_part_time=1;
    public static final int is_full_time=2;
    public static final int rate_per_hour=20;
-
+   public static final int working_days=20;
    public static void main(String[]  args){
 
        System.out.println("Welcome to Employee Wage Computation Program");
    int hours=0; 
    int wage=0;
-   double presence = Math.floor(Math.random()*10) % 3;
-       switch ((int)presence){
+   int netWage=0;
+   for(int day=0; day<working_days; day++){
+
+       int presence = (int) Math.floor(Math.random()*10) % 3;
+        switch (presence){
          case is_full_time:
             hours=8;
             break;
@@ -19,11 +22,11 @@ public class employee{
          default : 
             hours=0;
          }
-
-     wage= hours * rate_per_hour;
+     wage = hours * rate_per_hour;
+     netWage += wage;
+     }
     
-     System.out.println("Daily Wage Of Employee Is : " + wage);
+    System.out.println("Employee Wage for 20 working days :"+netWage);
+   }
 
-  }
-}
-
+ }

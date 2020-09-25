6ac6bb7e5f3f160094c01f5daf742c0a8651bd35
Nishant Chauhan <nishantkr0210@gmail.com>
2020-09-25 17:29:55 +0530

    Switch Case Used

--- employee.java
+++ employee.java
@@ -1,19 +1,26 @@
 public class employee{
+   public static final int is_part_time=1;
+   public static final int is_full_time=2;
+   public static final int rate_per_hour=20;
 
    public static void main(String[]  args){
-       System.out.println("Welcome to Employee Wage Computation Program");
-   int hours=0; int wage=0;
-   double presence = Math.floor(Math.random()*10) % 3;
-       if(presence==1){
-          hours=8;
-          }
-      else if(presence==2){
-       hours=4;
-          }
-      else
-       hours=0;
 
-     wage= hours*20;
+       System.out.println("Welcome to Employee Wage Computation Program");
+   int hours=0; 
+   int wage=0;
+   double presence = Math.floor(Math.random()*10) % 3;
+       switch ((int)presence){
+         case is_full_time:
+            hours=8;
+            break;
+         case is_part_time:
+            hours=4;
+            break;
+         default : 
+            hours=0;
+         }
+
+     wage= hours * rate_per_hour;
     
      System.out.println("Daily Wage Of Employee Is : " + wage);
 
